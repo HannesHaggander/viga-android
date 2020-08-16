6d9d4c8bb587195693ca4de15301fa5a4180df92
Hannes <h.haggander@gmail.com>
2020-08-16 10:43:44 +0200

    setup dependencies

--- app/src/main/java/com/towerowl/viga/MainActivity.kt
+++ app/src/main/java/com/towerowl/viga/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.towerowl.viga
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findNavController(R.id.main_navigation_graph).run { navController = this }
     }
 }
---
+++ app/src/main/java/com/towerowl/viga/misc/App.kt
@@ -0,0 +1,7 @@
+package com.towerowl.viga.misc
+
+import android.app.Application
+
+class App : Application() {
+
+}
